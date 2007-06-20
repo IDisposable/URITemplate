5cdedc408ef9fb2e69396abaa628b14baa3141bb
IDisposable_cp <SND\IDisposable_cp>
2007-06-20 20:44:37 +0000

    Merge in the cleanup of namespaces from the 2.0 framework version

--- Development/Framework 1.1/UriTemplate/Properties/AssemblyInfo.cs
+++ Development/Framework 1.1/UriTemplate/Properties/AssemblyInfo.cs
@@ -2,34 +2,14 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("UriTemplate")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("UriTemplating and pattern parsing IDisposable@gmail.com")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Phydeaux Software")]
 [assembly: AssemblyProduct("UriTemplate")]
-[assembly: AssemblyCopyright("Copyright ©  2007")]
+[assembly: AssemblyCopyright("Copyright © 2007, Marc C. Brooks")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("2bffffc6-992e-4f57-8981-e01e48d6bc40")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
--- Development/Framework 1.1/UriTemplate/UriPattern.cs
+++ Development/Framework 1.1/UriTemplate/UriPattern.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections;
 using System.Collections.Specialized;
-using System.Text;
 using System.Text.RegularExpressions;
 
 namespace UriTemplate
